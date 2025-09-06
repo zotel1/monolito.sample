2d8ac93771dc19cf4ced68468441ce1eee3d01c2
zotel1 <zotelsigel@gmail.com>
2025-09-06 19:47:08 -0300

    creamos nuestra clase instructor con sus parametros privados, su constructor, su getter y setter

--- src/main/java/com/fullstack/curso/entities/Instructor.java
+++ src/main/java/com/fullstack/curso/entities/Instructor.java
@@ -1,4 +1,40 @@
 package com.fullstack.curso.entities;
 
+import java.util.UUID;
+
 public class Instructor {
+
+    private UUID id;
+    private String nombre;
+    private String apellido;
+
+    public Instructor(String nombre, String apellido){
+        this.id = UUID.randomUUID();
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
 }
